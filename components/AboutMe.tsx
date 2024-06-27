8c868a23eea9047321d6c292411424909091d578
johnlim <ziyangjohn@gmail.com>
2024-06-27 16:20:45 +0800

    change about me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -23,7 +23,7 @@ const AboutMe = () => {
       </div>
       <div className="max-w-4xl text-center mb-10">
         <p className="text-lg">
-          Hello! I'm John, a Final Year Business Analytics and Computer Science Student from Nanyang Technological University. I am passionate software developer with experience in building web applications using modern technologies. I love coding, learning new skills, and going for hackathons. My goal is to create impactful solutions that make a difference.
+          Hello! I am John, a Final Year Business Analytics and Computer Science Student from Nanyang Technological University. I am passionate software developer with experience in building web applications using modern technologies. I love coding, learning new skills, and going for hackathons. My goal is to create impactful solutions that make a difference.
         </p>
       </div>
 
